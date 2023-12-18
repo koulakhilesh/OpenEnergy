4feed9a1104f0f90430ac4b9c98cb1a9399d3067
koulakhilesh <koulakhilesh@gmail.com>
2023-12-18 21:10:55 +0000

    added updated battery model - wip

--- scripts/battery.py
+++ scripts/battery.py
@@ -4,12 +4,11 @@ class Battery:
         capacity_mwh: float,
         charge_efficiency: float = 0.9,
         discharge_efficiency: float = 0.9,
-        max_charge_rate=None,
-        max_discharge_rate=None,
+        max_charge_rate_mw=None,
+        max_discharge_rate_mw=None,
         initial_soh: float = 1.0,
         initial_soc: float = 0.0,
         starting_cycle_count: float = 0.0
-        
     ):
         if capacity_mwh <= 0:
             raise ValueError("Capacity must be greater than 0")
@@ -23,41 +22,49 @@ class Battery:
         self.discharge_efficiency = discharge_efficiency
         self.soc = initial_soc  # Initial state of charge as a fraction (0 to 1)
         self.soh = initial_soh
-        self.max_charge_rate = max_charge_rate if max_charge_rate else capacity_mwh
-        self.max_discharge_rate = (
-            max_discharge_rate if max_discharge_rate else capacity_mwh
+        self.max_charge_rate_mw = max_charge_rate_mw if max_charge_rate_mw else capacity_mwh
+        self.max_discharge_rate_mw = (
+            max_discharge_rate_mw if max_discharge_rate_mw else capacity_mwh
         )
         self.cycle_count = starting_cycle_count
         self.last_cycle_soc = initial_soc  # Keep track of SOC at the last cycle
+        self.energy_cycled_mwh = 0.0  # Track total energy cycled
 
+    def charge(self, energy_mwh: float, duration_hours=0.5):
+        # Calculate the actual charge based on energy and efficiency
+        charge_energy_mwh = min(energy_mwh, self.max_charge_rate_mw * duration_hours)
+        actual_charge_energy_mwh = charge_energy_mwh * self.charge_efficiency
 
-    def charge(self, mwh: float, duration_hours=0.5):
-        charge_amount = min(mwh, self.max_charge_rate * duration_hours)
-        actual_charge = charge_amount * self.charge_efficiency
-        self.soc = min(self.soc + actual_charge / self.capacity_mwh, 1.0)
-        energy_cycled = charge_amount * duration_hours
-        dod = self.soc
-        self.update_soh(energy_cycled, dod)
+        # Update SOC and energy_cycled
+        self.soc = min(self.soc + actual_charge_energy_mwh / self.capacity_mwh, 1.0)
+        self.energy_cycled_mwh += charge_energy_mwh
+
+        # Calculate DoD and update SOH and cycle count
+        dod = 1.0 - self.soc
+        self.update_soh(charge_energy_mwh, dod)
         self.check_and_update_cycles()
 
+    def discharge(self, energy_mwh: float, duration_hours=0.5):
+        # Calculate the actual discharge based on energy and efficiency
+        discharge_energy_mwh = min(energy_mwh, self.max_discharge_rate_mw * duration_hours)
+        actual_discharge_energy_mwh = discharge_energy_mwh * self.discharge_efficiency
 
-    def discharge(self, mwh: float, duration_hours=0.5):
-        discharge_amount = min(mwh, self.max_discharge_rate * duration_hours)
-        actual_discharge = discharge_amount * self.discharge_efficiency
-        self.soc = max(self.soc - actual_discharge / self.capacity_mwh, 0.0)
-        energy_cycled = discharge_amount * duration_hours
-        dod = self.soc
-        self.update_soh(energy_cycled, dod)
+        # Update SOC and energy_cycled
+        self.soc = max(self.soc - actual_discharge_energy_mwh / self.capacity_mwh, 0.0)
+        self.energy_cycled_mwh += discharge_energy_mwh
+
+        # Calculate DoD and update SOH and cycle count
+        dod = 1.0 - self.soc
+        self.update_soh(discharge_energy_mwh, dod)
         self.check_and_update_cycles()
 
-    
     def check_and_update_cycles(self):
-        """
-        Check and update the cycle count based on SOC changes.
-        """
-        if (self.last_cycle_soc <= 0.2 and self.soc >= 0.8) or (self.last_cycle_soc >= 0.8 and self.soc <= 0.2):
+        # Check for significant cycles based on energy_cycled_mwh
+        # Update cycle_count accordingly
+        if self.energy_cycled_mwh >= self.capacity_mwh:
             self.cycle_count += 1
-            self.last_cycle_soc = self.soc    
+            self.energy_cycled_mwh -= self.capacity_mwh
+        self.last_cycle_soc = self.soc
 
     def get_soc(self) -> float:
         """Return the current state of charge."""
@@ -71,7 +78,9 @@ class Battery:
         """
         Update the State of Health (SOH) of the battery.
         """
-        degradation_rate = self.calculate_degradation_rate(energy_cycled_mwh, dod)
+        base_degradation = 0.00005  # Base degradation rate per MWh cycled
+        dod_factor = 2 if dod > 0.5 else 1  # Higher degradation if DoD is more than 50%
+        degradation_rate = base_degradation * energy_cycled_mwh * dod_factor
         self.soh *= 1 - degradation_rate
 
     def calculate_degradation_rate(self, energy_cycled_mwh: float, dod: float) -> float:
